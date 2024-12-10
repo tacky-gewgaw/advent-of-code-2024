81de71b3efa638ee724517368c91a1e5caba37d9
Sander van Kasteel <22967311+tacky-gewgaw@users.noreply.github.com>
2024-12-10 23:03:37 +0100

    add answer for day 3.2

--- src/day3/main.jl
+++ src/day3/main.jl
@@ -1,9 +1,30 @@
 inputfile = joinpath(pwd(),"src","day3","input.txt")
 
-input = readlines(inputfile)
+input = read(inputfile, String)
 
 re = r"mul\((\d{1,3}),(\d{1,3})\)"
 
-answer1 = sum([ sum([ parse(Int64,n[1]) * parse(Int64,n[2]) for n in eachmatch(re, line) ]) for line in input ])
+answer1 = sum([ parse(Int64,n[1]) * parse(Int64,n[2]) for n in eachmatch(re, input) ])
 
-print(answer1)
+println(answer1)
+
+re2 = r"mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)"
+
+matches = eachmatch(re2, input)
+
+answer2 = 0
+enabled = true
+
+for m in matches
+    if m.match == "do()"
+        enabled = true
+    elseif m.match == "don't()"
+        enabled = false
+    else
+        if enabled
+            answer2 += parse(Int64,m[1])*parse(Int64,m[2])
+        end
+    end
+end
+
+println(answer2)
