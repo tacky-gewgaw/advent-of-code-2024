15e5b3d62fbb2d01846e82950aee87fa0ce85de7
Sander van Kasteel <22967311+tacky-gewgaw@users.noreply.github.com>
2024-12-18 17:18:04 +0100

    7.2 thanks, I hate it

--- src/day7/main.jl
+++ src/day7/main.jl
@@ -12,7 +12,14 @@ end
 
 equations = parse_equation.(input)
 
-function check_equation(e::Tuple{Int,Vector{Int}})
+function conc(x,y)
+    xs = string(x)
+    ys = string(y)
+    res = xs * ys
+    return parse(Int,res)
+end
+
+function check_equation(e::Tuple{Int,Vector{Int}}, concat::Bool=false)
     if e[2] == []
         println("1")
         return false
@@ -29,7 +36,10 @@ function check_equation(e::Tuple{Int,Vector{Int}})
         return x == e[1]
     else
         (y, tail) = Iterators.peel(rest)
-        return check_equation((e[1],vcat([x+y],collect(tail)))) | check_equation((e[1],vcat([x*y],collect(tail))))
+        return check_equation((e[1],vcat([x+y],collect(tail))),concat) |
+                check_equation((e[1],vcat([x*y],collect(tail))),concat) | (
+                    concat ? check_equation((e[1],vcat([conc(x,y)],collect(tail))),concat) : false
+                )
     end
 
 
@@ -41,4 +51,11 @@ start_time = Dates.now()
 println(sum(first.(filter(check_equation,equations))))
 
 running_time = Dates.now() - start_time
+println("Calculation took $running_time")
+
+start_time1 = Dates.now()
+
+println(sum(first.(filter(t -> check_equation(t,true),equations))))
+
+running_time1 = Dates.now() = start_time1
 println("Calculation took $running_time")
